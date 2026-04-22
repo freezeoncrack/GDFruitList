ddea93bb4f8d2450617e29bec60bf8b486bdecf1
Freeze <freezeyaboy@gmail.com>
2026-04-22 16:24:39 -0700

    Fix quotes and update base path in vite config

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,17 @@
-import { defineConfig } from "vite";
-import { resolve } from "path";
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   build: {
     rollupOptions: {
       input: {
-        main: resolve(__dirname, "index.html"),
-        list: resolve(__dirname, "list.html"),
-        admin: resolve(__dirname, "admin.html"),
-        users: resolve(__dirname, "users.html"),
-        userleaderboard: resolve(__dirname, "userleaderboard.html"),
+        main: resolve(__dirname, 'index.html'),
+        list: resolve(__dirname, 'list.html'),
+        users: resolve(__dirname, 'users.html'),
+        admin: resolve(__dirname, 'admin.html'),
+        userleaderboard: resolve(__dirname, 'userleaderboard.html'),
       },
     },
   },
-});
+})
